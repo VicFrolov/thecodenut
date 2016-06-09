4d6a9b57f4ac87c458f1ea8617ce1dec83e27e59
Victor Frolov <vicfrolov@gmail.com>
2016-06-08 18:01:33 -0700

    added some css restrictions on width, implemented show/hide all button, and added some information on polymorphism and inheritance

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,23 @@ $(document).ready (function () {
     var toggleColorNotVisible = "rgb(255, 0, 0)";
     var toggleColorVisible = "rgb(0, 0, 0)";
     var backgroundColorVisible = "rgb(255, 255, 255)";
+    var showAnswers = false;
+    var showAnswersButton = false;
+
+    $("#keywordButton").click(function () {
+        if (showAnswersButton) {
+            $(".answer").css("color", toggleColorNotVisible);
+            $(".answer").css("background-color", toggleColorNotVisible);
+        } else {
+            $(".answer").css("color", toggleColorVisible);
+            $(".answer").css("background-color", backgroundColorVisible);
+
+        }
+        showAnswersButton = !showAnswersButton;
+    });
 
     $(".answer").click(function () {
-        if ($(this).css("color") === toggleColorVisible) {
+        if (showAnswers) {
             $(this).css("color", toggleColorNotVisible);
             $(this).css("background-color", toggleColorNotVisible);
         } else {
@@ -12,6 +26,7 @@ $(document).ready (function () {
             $(this).css("background-color", backgroundColorVisible);
 
         }
+        showAnswers = !showAnswers;
     });
     
 })
