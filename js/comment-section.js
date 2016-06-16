87a5b627ca6674ed7c9b2a9532fedbbae7aa3f3c
Victor Frolov <vicfrolov@gmail.com>
2016-06-16 04:44:34 -0700

    moved most of the code from the commentsRef stream to the showComments as it is more related to injecting the values

--- js/comment-section.js
+++ js/comment-section.js
@@ -1,74 +1,65 @@
 var showJavaQComments = function() {
     var config = {
-      apiKey: "AIzaSyDiELrcysQjtMGCBYZj9p-9R-3n4mIobIg",
-      authDomain: "the-code-nut.firebaseapp.com",
-      databaseURL: "https://the-code-nut.firebaseio.com",
-      storageBucket: "the-code-nut.appspot.com",
+        apiKey: "AIzaSyDiELrcysQjtMGCBYZj9p-9R-3n4mIobIg",
+        authDomain: "the-code-nut.firebaseapp.com",
+        databaseURL: "https://the-code-nut.firebaseio.com",
+        storageBucket: "the-code-nut.appspot.com",
     };
+
     firebase.initializeApp(config);
     var database = firebase.database();
     var commentsRef = database.ref('comments/')
 
     var writeComment = function (userName, newComment, time) {
-      database.ref('comments/').push({
-        userName: userName,
-        comment: newComment,
-        timeStamp: time
-      });
+        database.ref('comments/').push({
+            userName: userName,
+            comment: newComment,
+            timeStamp: time
+        });
     }
 
     $("#commentsContent").on("click", "#javaQaddComment", function() {
-      var username = $("#usernameInput").val();
-      var newComment = $("#commentInput").val();
-      var time = timeStamp();
+        var username = $("#usernameInput").val();
+        var newComment = $("#commentInput").val();
+        var time = timeStamp();
 
-      if (username && newComment) {
-        writeComment(username, newComment, time);
-        $("#usernameInput").val('');
-        $("#commentInput").val('');
-      } else {
-        alert("please enter a username and comment");
-      }
+        if (username && newComment) {
+            writeComment(username, newComment, time);
+            $("#usernameInput").val('');
+            $("#commentInput").val('');
+        } else {
+            alert("please enter a username and comment");
+        }
     });
 
-    commentsRef.on('value', function(snapshot) {
-      var comments = snapshot.val();
-      $.each(comments, function () {
-        showComments(this.userName, this.comment, this.timeStamp);
-      });
-    });
-
-    var showComments = function(username, comment, timeStamp) {
-      console.log(username + " " + comment);
-      $("#javaQcomments-section").append('<div>' + username + '<br>' + comment + '<br>' + timeStamp +
-        '</div>');
+    var showComments = function (jsonss) {
+        var html = '';
+        $.each(json, function () {
+            html += '<div>' + this.userName + '<br>' + this.comment + '<br>' + this.timeStamp + '</div>';
+        });
+        $("#javaQcomments-section").html(html);
     }
+
+    commentsRef.orderByPriority().on('value', function(snapshot) {
+        var comments = snapshot.val();
+        showComments(comments);
+    });
+
 
     // Thanks to https://gist.github.com/hurjas/2660489#file-timestamp-js-L26
     function timeStamp() {
-      var now = new Date();
-      var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
-      var time = [now.getHours(), now.getMinutes()];
-      var suffix = (time[0] < 12) ? "AM" : "PM";
-      time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+        var now = new Date();
+        var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+        var time = [now.getHours(), now.getMinutes()];
+        var suffix = (time[0] < 12) ? "AM" : "PM";
+        time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
 
-      for (var i = 1; i < 3; i++) {
-        if (time[i] < 10) {
-          time[i] = "0" + time[i];
+        for (var i = 1; i < 3; i++) {
+            if (time[i] < 10) {
+              time[i] = "0" + time[i];
+            }
         }
+
+        return date.join("/") + ", " + time.join(":") + " " + suffix;
       }
-
-      return date.join("/") + ", " + time.join(":") + " " + suffix;
-    }
-
-    //
-    // ref.on("child_added", function(snapshot) {
-    //   var comment = snapshot.val();
-    //   addComment(comment.name, comment.comment, comment.time);
-    // });
-    //
-    // function addComment(name, comment, timeStamp) {
-    //   var comments = document.getElementById("comments");
-    //   comments.innerHTML = "<hr><h4>" + name + " says<span>" + timeStamp + "</span></h4><p>" + comment + "</p>" + comments.innerHTML;
-    // }
 };
